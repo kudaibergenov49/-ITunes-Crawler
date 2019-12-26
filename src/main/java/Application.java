1e77a73b3f2fb2d21ef9e0b9d1a9fd60ec4a602a
Kuanysh <kudaibergenov49@gmail.com>
2019-12-26 14:50:35 +0300

    Update Application.java
    Мелкий рефакторинг

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,15 +6,8 @@ public class Application {
     public static void main(String[] args) {
         long time = System.currentTimeMillis();
         DBConnector connector = new DBConnector();
-        //List<String> applications = new Crawler().getAllData();
-        List<String> applications = new ArrayList<>();
-        for (int i = 0; i < 2000; i++) {
-            applications.add(String.valueOf(i));
-        }
-        applications.add("ddd");
-        applications.add("eee");
         connector.createTable();
-        connector.save(applications);
+        connector.save(new Crawler().getAllData());
         System.out.println(System.currentTimeMillis() - time);
     }
 }
